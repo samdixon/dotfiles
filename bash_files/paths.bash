7b02d491d0157c0a8a74a442a05e014f14db81e7
samdixon <sdixon@posteo.net>
2019-11-25 21:40:18 -0500

    removes rvm paths, adds python binaries

--- bash_files/paths.bash
+++ bash_files/paths.bash
@@ -1,12 +1,9 @@
 # Path Stuffs
+# Go
 export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:$HOME/go/bin
-
-# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
-export PATH="$PATH:$HOME/.rvm/bin"
-
-export PATH=/usr/local/opt/ruby/bin:$PATH
-export PATH=/usr/local/lib/ruby/gems/2.6.0/bin:$PATH
+# Python
+export PATH=$PATH:$HOME/.local/bin
 
 # Env vars
 export EDITOR=nvim
