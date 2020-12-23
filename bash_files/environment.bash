fa46b0915921cfeafb5713c622aeb778ac3aba5b
samdixon <sdixon@posteo.net>
2020-12-23 12:13:16 -0500

    Removes outdated paths, dircolors eval

--- bash_files/environment.bash
+++ bash_files/environment.bash
@@ -1,6 +1,5 @@
 # Path Stuffs
-export PATH=$PATH:~/bin:$HOME/.local/bin:/usr/local/go/bin:$HOME/go/bin:$HOME/.npm-packages/bin
+export PATH=$PATH:$HOME/bin:$HOME/.local/bin
 
 # Env vars
 export EDITOR=vim
-eval `dircolors /home/sdixon/.dir_colors/dircolors`
