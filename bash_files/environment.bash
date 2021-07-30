8fa7dff30658888c7e0a4898aaec8688e9ed6851
samdixon <sdixon@posteo.net>
2021-07-30 19:49:53 -0400

    updates environment variables

--- bash_files/environment.bash
+++ bash_files/environment.bash
@@ -1,5 +1,7 @@
-# Path Stuffs
-export PATH=$PATH:$HOME/bin:$HOME/.local/bin
-
-# Env vars
-export EDITOR=vim
+# env
+export PATH=/opt/homebrew/bin:$PATH:$HOME/bin:$HOME/.npm-packages/bin
+export EDITOR=nvim
+alias vim='nvim'
+alias awsume='. awsume'
+# prompt
+PS1=$'\[\033[01;95m\]\u@\h\[\033[00m\]: \[\033[01;34m\]\w\n\[\033[00m\]$ '
