d3940c4b849d2b073c2e77a5da9334a5e01e3dba
samdixon <sdixon@posteo.net>
2021-08-11 21:58:51 -0400

    Updates environ, sourced files, bell noise

--- bash_files/environment.bash
+++ bash_files/environment.bash
@@ -1,7 +1,25 @@
 # env
-export PATH=/opt/homebrew/bin:$PATH:$HOME/bin:$HOME/.npm-packages/bin
+export PATH="/opt/homebrew/bin:$PATH:$HOME/bin:$HOME/.npm-packages/bin:$HOME/.local/bin"
 export EDITOR=nvim
+export BASH_SILENCE_DEPRECATION_WARNING=1
+export notes_dir="/Users/samdixon/sjd/notes"
+#alias
 alias vim='nvim'
+alias a='. awsume'
 alias awsume='. awsume'
+alias awesume='. awsume'
+alias ls='ls -G'
 # prompt
 PS1=$'\[\033[01;95m\]\u@\h\[\033[00m\]: \[\033[01;34m\]\w\n\[\033[00m\]$ '
+# pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PATH:$PYENV_ROOT/bin"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+# nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# shell completions
+[ -f /usr/local/bin/aws_completer ] && complete -C '/usr/local/bin/aws_completer' aws
--- bashrc
+++ bashrc
@@ -1,4 +1,4 @@
-[ -f ~/.bash_files/fzf.bash ] && source ~/.bash_files/fzf.bash
 [ -f ~/.bash_files/defaults ] && source ~/.bash_files/defaults
 [ -f ~/.bash_files/functions.bash ] && source ~/.bash_files/functions.bash
 [ -f ~/.bash_files/environment.bash ] && source ~/.bash_files/environment.bash
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
