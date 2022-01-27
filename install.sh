900f1283371cec2048a78a11ddfaec51d18f3731
samdixon <sdixon@posteo.net>
2022-01-26 23:49:51 -0500

    updates

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 rm -rf ~/.bash* \
 	~/.zsh* \
 	~/.profile
@@ -13,8 +12,10 @@ fi
 
 cd ~/.dotfiles
 for f in .???*; do
-	rm -f ~/$f
-	(cd ~/; ln -s .dotfiles/$f $f)
+    if [[ $f != ".git" ]]; then
+        rm -f ~/$f
+        (cd ~/; ln -s .dotfiles/$f $f)
+    fi
 done
 
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
@@ -27,127 +28,129 @@ if [ ! -d ~/.fzf ]; then
 	~/.fzf/install
 fi
 
-xcode-select --install
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    xcode-select --install
+    SHELL_PATH="/opt/homebrew/bin/bash"
+    if [[ $(cat $SHELL_PATH | grep $SHELL_PATH) ]]; then
+        echo $SHELL_PATH | sudo tee -a /etc/shells
+        chsh -s $SHELL_PATH
+    fi
 
-echo "/opt/homebrew/bin/bash" | sudo tee -a /etc/shells
-chsh -s /opt/homebrew/bin/bash
+    # most from https://github.com/mathiasbynens/dotfiles/blob/main/.macos
+    # personal modifications at top
+    sudo -v 
 
-# a lot of this is copied from https://github.com/mathiasbynens/dotfiles/blob/main/.macos
-# but I don't like it all so some is removed and have added my own customizations
-# customizations at top
-sudo -v 
+    # Set a pretty fast keyboard repeat rate
+    defaults write NSGlobalDomain KeyRepeat -int 2
+    defaults write NSGlobalDomain InitialKeyRepeat -int 15 
 
-# Set a pretty fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 8 
+    # Terminal and terminal profile
+    open ~/.dotfiles/sjd.terminal
+    defaults write com.apple.dock "Default Window Settings" -string "sjd"
+    defaults write com.apple.dock "Startup Window Settings" -string "sjd"
 
-# Terminal and terminal profile
-open ~/.dotfiles/sjd.terminal
-defaults write com.apple.dock "Default Window Settings" -string "sjd"
-defaults write com.apple.dock "Startup Window Settings" -string "sjd"
+    # Dock position, hiding, size
+    defaults write com.apple.dock orientation -string "left"
+    defaults write com.apple.dock autohide -bool true
+    defaults write com.apple.dock tilesize -int 36
 
-# Dock position, hiding, size
-defaults write com.apple.dock orientation -string "left"
-defaults write com.apple.dock autohide -bool true
-defaults write com.apple.dock tilesize -int 36
+    # always show scrollbar
+    defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
 
-# always show scrollbar
-defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
+    # scroll direction
+    defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
-# scroll direction
-defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+    # Disable press-and-hold for keys in favor of key repeat
+    defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
-# Disable press-and-hold for keys in favor of key repeat
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+    # Require password immediately after sleep or screen saver begins
+    defaults write com.apple.screensaver askForPassword -int 1
+    defaults write com.apple.screensaver askForPasswordDelay -int 0
 
+    # Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
+    defaults write com.apple.finder QuitMenuItem -bool true
 
-# Require password immediately after sleep or screen saver begins
-defaults write com.apple.screensaver askForPassword -int 1
-defaults write com.apple.screensaver askForPasswordDelay -int 0
+    # Finder: disable window animations and Get Info animations
+    defaults write com.apple.finder DisableAllAnimations -bool true
 
-# Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
-defaults write com.apple.finder QuitMenuItem -bool true
+    # Set Home as the default location for new Finder windows
+    defaults write com.apple.finder NewWindowTarget -string "PfHm"
+    defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
 
-# Finder: disable window animations and Get Info animations
-defaults write com.apple.finder DisableAllAnimations -bool true
+    # Show icons for hard drives, servers, and removable media on the desktop
+    defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+    defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+    defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+    defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
-# Set Home as the default location for new Finder windows
-defaults write com.apple.finder NewWindowTarget -string "PfHm"
-defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
+    # Finder: show all filename extensions
+    defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-# Show icons for hard drives, servers, and removable media on the desktop
-defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
-defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
-defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+    # Finder: show status bar
+    defaults write com.apple.finder ShowStatusBar -bool true
 
-# Finder: show all filename extensions
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+    # Finder: show path bar
+    defaults write com.apple.finder ShowPathbar -bool true
 
-# Finder: show status bar
-defaults write com.apple.finder ShowStatusBar -bool true
+    # Display full POSIX path as Finder window title
+    defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
-# Finder: show path bar
-defaults write com.apple.finder ShowPathbar -bool true
+    # Keep folders on top when sorting by name
+    defaults write com.apple.finder _FXSortFoldersFirst -bool true
 
-# Display full POSIX path as Finder window title
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+    # When performing a search, search the current folder by default
+    defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
-# Keep folders on top when sorting by name
-defaults write com.apple.finder _FXSortFoldersFirst -bool true
+    # Disable the warning when changing a file extension
+    defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-# When performing a search, search the current folder by default
-defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+    # Enable spring loading for directories
+    defaults write NSGlobalDomain com.apple.springing.enabled -bool true
 
-# Disable the warning when changing a file extension
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+    # Remove the spring loading delay for directories
+    defaults write NSGlobalDomain com.apple.springing.delay -float 0
 
-# Enable spring loading for directories
-defaults write NSGlobalDomain com.apple.springing.enabled -bool true
+    # Change minimize/maximize window effect
+    defaults write com.apple.dock mineffect -string "scale"
 
-# Remove the spring loading delay for directories
-defaults write NSGlobalDomain com.apple.springing.delay -float 0
+    # Minimize windows into their application’s icon
+    defaults write com.apple.dock minimize-to-application -bool true
 
-# Change minimize/maximize window effect
-defaults write com.apple.dock mineffect -string "scale"
+    # Show indicator lights for open applications in the Dock
+    defaults write com.apple.dock show-process-indicators -bool true
 
-# Minimize windows into their application’s icon
-defaults write com.apple.dock minimize-to-application -bool true
+    # Wipe all (default) app icons from the Dock
+    # This is only really useful when setting up a new Mac, or if you don’t use
+    # the Dock to launch apps.
+    defaults write com.apple.dock persistent-apps -array
 
-# Show indicator lights for open applications in the Dock
-defaults write com.apple.dock show-process-indicators -bool true
+    # Show only open applications in the Dock
+    defaults write com.apple.dock static-only -bool true
 
-# Wipe all (default) app icons from the Dock
-# This is only really useful when setting up a new Mac, or if you don’t use
-# the Dock to launch apps.
-defaults write com.apple.dock persistent-apps -array
+    # Don’t animate opening applications from the Dock
+    defaults write com.apple.dock launchanim -bool false
 
-# Show only open applications in the Dock
-defaults write com.apple.dock static-only -bool true
+    # Don’t show recent applications in Dock
+    defaults write com.apple.dock show-recents -bool false
 
-# Don’t animate opening applications from the Dock
-defaults write com.apple.dock launchanim -bool false
+    # Disable send and reply animations in Mail.app
+    defaults write com.apple.mail DisableReplyAnimations -bool true
+    defaults write com.apple.mail DisableSendAnimations -bool true
 
-# Don’t show recent applications in Dock
-defaults write com.apple.dock show-recents -bool false
+    # Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
+    defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 
-# Disable send and reply animations in Mail.app
-defaults write com.apple.mail DisableReplyAnimations -bool true
-defaults write com.apple.mail DisableSendAnimations -bool true
+    # Add the keyboard shortcut ⌘ + Enter to send an email in Mail.app
+    defaults write com.apple.mail NSUserKeyEquivalents -dict-add "Send" "@\U21a9"
 
-# Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
-defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+    # Display emails in threaded mode, sorted by date (oldest at the top)
+    defaults write com.apple.mail DraftsViewerAttributes -dict-add "DisplayInThreadedMode" -string "yes"
+    defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortedDescending" -string "yes"
+    defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortOrder" -string "received-date"
 
-# Add the keyboard shortcut ⌘ + Enter to send an email in Mail.app
-defaults write com.apple.mail NSUserKeyEquivalents -dict-add "Send" "@\U21a9"
+    # Disable inline attachments (just show the icons)
+    defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
 
-# Display emails in threaded mode, sorted by date (oldest at the top)
-defaults write com.apple.mail DraftsViewerAttributes -dict-add "DisplayInThreadedMode" -string "yes"
-defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortedDescending" -string "yes"
-defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortOrder" -string "received-date"
-
-# Disable inline attachments (just show the icons)
-defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
-
-# Disable automatic spell checking
-defaults write com.apple.mail SpellCheckingBehavior -string "NoSpellCheckingEnabled"
+    # Disable automatic spell checking
+    defaults write com.apple.mail SpellCheckingBehavior -string "NoSpellCheckingEnabled"
+fi
